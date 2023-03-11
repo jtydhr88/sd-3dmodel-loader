86d52c3c18d9a3558171e039321ab0825b05c3c9
missionfloyd <missionfloyd@users.noreply.github.com>
2023-03-11 00:52:32 -0700

    Clean up javascript formatting

--- javascript/main.js
+++ javascript/main.js
@@ -12,7 +12,6 @@ var mainObject;
 
 function updateModel(modelScalePage) {
     var object = scene.getObjectByName("mainObject");
-
     var modelScale = Number(modelScalePage);
 
     if (object) {
@@ -34,10 +33,8 @@ function setAxisVisible(hasAxis) {
 
 function setCanvasSize(width, height) {
     renderer.setSize(width, height);
-
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
-
     webGLOutputDiv.setAttribute("style", `width: ${width + 2}px; height: ${height + 2}px; border: 0.5px solid;`);
 }
 
@@ -52,12 +49,8 @@ function setGroundColor(gColor) {
 function playAndPause() {
     if (isPlay) {
         isPlay = false;
-    }
-    else {
-        if (action) {
-            action.play();
-        }
-
+    } else if (action) {
+        action.play();
         isPlay = true;
     }
 }
@@ -66,7 +59,6 @@ function stop() {
     if (action) {
         action.stop();
     }
-
     isPlay = false;
 }
 
@@ -82,100 +74,79 @@ function uploadFile() {
     const input = document.createElement("input");
     input.type = "file"
     input.accept = ".obj, .stl, .fbx"
-    input.addEventListener("change", function(e){
+    input.addEventListener("change", function(e) {
         const file = e.target.files[0];
-
         var filename = file.name;
-		var extension = filename.split( '.' ).pop().toLowerCase();
-
-		var reader = new FileReader();
-
+        var extension = filename.split('.').pop().toLowerCase();
+        var reader = new FileReader();
         var manager = new THREE.LoadingManager();
 
         removeMainObject();
 
-        switch ( extension ) {
+        switch (extension) {
             case 'obj':
-                reader.addEventListener( 'load', function ( event ) {
-					var contents = event.target.result;
-
-					mainObject = new THREE.OBJLoader().parse( contents );
-					mainObject.name = "mainObject";
-
-					scene.add(mainObject);
-
-				}, false );
-				reader.readAsText( file );
-
-                break;
-            case 'stl':
-                reader.addEventListener( 'load', function ( event ) {
+                reader.addEventListener('load', function(event) {
                     var contents = event.target.result;
 
-                    var geometry = new THREE.STLLoader().parse( contents );
+                    mainObject = new THREE.OBJLoader().parse(contents);
+                    mainObject.name = "mainObject";
+                    scene.add(mainObject);
+                }, false);
+
+                reader.readAsText(file);
+                break;
+            case 'stl':
+                reader.addEventListener('load', function(event) {
+                    var contents = event.target.result;
+                    var geometry = new THREE.STLLoader().parse(contents);
+                    var material = new THREE.MeshStandardMaterial();
 
                     geometry.sourceType = "stl";
                     geometry.sourceFile = file.name;
 
-					var material = new THREE.MeshStandardMaterial();
+                    mainObject = new THREE.Mesh(geometry, material);
+                    mainObject.name = "mainObject";
 
-					mainObject = new THREE.Mesh( geometry, material );
-					mainObject.name = "mainObject";
+                    scene.add(mainObject);
+                }, false);
 
-					scene.add(mainObject);
+                if (reader.readAsBinaryString !== undefined) {
+                    reader.readAsBinaryString(file);
+                } else {
+                    reader.readAsArrayBuffer(file);
+                }
 
-				}, false );
-
-				if ( reader.readAsBinaryString !== undefined ) {
-					reader.readAsBinaryString( file );
-
-				}
-				else {
-					reader.readAsArrayBuffer( file );
-				}
-
-				break;
+                break;
 
             case 'fbx':
-                reader.addEventListener( 'load', function ( event ) {
-
-					var contents = event.target.result;
-
-					var loader = new THREE.FBXLoader( manager );
-					mainObject = loader.parse( contents );
-
-					mixer = new THREE.AnimationMixer( mainObject );
+                reader.addEventListener('load', function(event) {
+                    var contents = event.target.result;
+                    var loader = new THREE.FBXLoader(manager);
+                    mainObject = loader.parse(contents);
+                    mixer = new THREE.AnimationMixer(mainObject);
 
                     if (mainObject.animations[0]) {
-                        action = mixer.clipAction( mainObject.animations[ 0 ] );
+                        action = mixer.clipAction(mainObject.animations[0]);
 
                         action.play();
                     }
 
-                    mainObject.traverse( function ( child ) {
-						if ( child.isMesh ) {
+                    mainObject.traverse(function(child) {
+                        if (child.isMesh) {
+                            child.castShadow = true;
+                            child.receiveShadow = true;
+                        }
+                    });
 
-							child.castShadow = true;
-							child.receiveShadow = true;
+                    mainObject.name = "mainObject";
+                    scene.add(mainObject);
+                }, false);
 
-						}
-
-					} );
-
-					mainObject.name = "mainObject";
-
-                    scene.add( mainObject );
-
-				}, false );
-
-				reader.readAsArrayBuffer( file );
-
-				break;
+                reader.readAsArrayBuffer(file);
+                break;
         }
     })
-
     input.click();
-
     isPlay = true;
 }
 
@@ -189,38 +160,33 @@ function resetCamera() {
     camera.position.x = -30;
     camera.position.y = 40;
     camera.position.z = 30;
-
     camera.lookAt(scene.position);
 }
 
 function initWebGLOutput(webGLOutputDiv) {
     // create a scene, that will hold all our elements such as objects, cameras and lights.
     scene = new THREE.Scene();
-
     var clock = new THREE.Clock();
 
-    light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-    light.position.set( 0, 200, 0 );
-    scene.add( light );
+    light = new THREE.HemisphereLight(0xffffff, 0x444444);
+    light.position.set(0, 200, 0);
+    scene.add(light);
 
-    light = new THREE.DirectionalLight( 0xffffff );
-    light.position.set( 0, 200, 100 );
+    light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(0, 200, 100);
     light.castShadow = true;
     light.shadow.camera.top = 180;
-    light.shadow.camera.bottom = - 100;
-    light.shadow.camera.left = - 120;
+    light.shadow.camera.bottom = -100;
+    light.shadow.camera.left = -120;
     light.shadow.camera.right = 120;
-    scene.add( light );
+    scene.add(light);
 
     var width = webGLOutputDiv.getAttribute("canvas_width");
     var height = webGLOutputDiv.getAttribute("canvas_height");
 
     camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
-
     resetCamera();
-
     var orbit = new THREE.OrbitControls(camera);
-
     orbit.enabled = false;
 
     renderer = new THREE.WebGLRenderer({
@@ -241,33 +207,37 @@ function initWebGLOutput(webGLOutputDiv) {
 
     if (hasAxis == 'True') {
         axes.visible = true;
-    }
-    else {
+    } else {
         axes.visible = false;
     }
 
-    groundMesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0xEEEEEE, depthWrite: false } ) );
+    groundMesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshPhongMaterial({
+        color: 0xEEEEEE,
+        depthWrite: false
+    }));
 
-    groundMesh.rotation.x = - Math.PI / 2;
+    groundMesh.rotation.x = -Math.PI / 2;
     groundMesh.receiveShadow = true;
+    scene.add(groundMesh);
 
-    scene.add( groundMesh );
-
-    groundGrid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
-
+    groundGrid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
     groundGrid.material.opacity = 0.2;
     groundGrid.material.transparent = true;
-
-    scene.add( groundGrid );
+    scene.add(groundGrid);
 
     var hasGround = webGLOutputDiv.getAttribute("has_ground");
 
     if (hasGround == 'True') {
         groundMesh.visible = true;
-        groundGrid.visible = true;
-    }
-    else {
+    } else {
         groundMesh.visible = false;
+    }
+    
+    var hasGroundGrid = webGLOutputDiv.getAttribute("has_ground_grid");
+
+    if (hasGroundGrid == 'True') {
+        groundGrid.visible = true;
+    } else {
         groundGrid.visible = false;
     }
 
@@ -290,12 +260,10 @@ function initWebGLOutput(webGLOutputDiv) {
     );
 
     isPlay = true;
-
     render();
 
     function render() {
         orbit.update();
-
         requestAnimationFrame(render);
 
         var delta = clock.getDelta();
@@ -308,22 +276,17 @@ function initWebGLOutput(webGLOutputDiv) {
     }
 }
 
-function sendImage(type, index){
+function sendImage(type, index) {
     renderer.domElement.toBlob((blob) => {
         const file = new File([blob], "pose.png")
-
         const dt = new DataTransfer();
-
         dt.items.add(file);
-
         const list = dt.files
-
         const selector = type === "txt2img" ? "#txt2img_script_container" : "#img2img_script_container"
 
-        if (type === "txt2img"){
+        if (type === "txt2img") {
             switch_to_txt2img()
-        }
-        else if(type === "img2img"){
+        } else if (type === "img2img") {
             switch_to_img2img()
         }
 
@@ -345,7 +308,10 @@ function sendImage(type, index){
         }
         input.value = "";
         input.files = list;
-        const event = new Event('change', { 'bubbles': true, "composed": true });
+        const event = new Event('change', {
+            'bubbles': true,
+            "composed": true
+        });
         input.dispatchEvent(event);
     });
 }
@@ -354,16 +320,16 @@ let executed_webGL_output = false;
 
 window.addEventListener('DOMContentLoaded', () => {
     const observer = new MutationObserver((m) => {
-        if(!executed_webGL_output && gradioApp().querySelector('#WebGL-output')){
+        if (!executed_webGL_output && gradioApp().querySelector('#WebGL-output')) {
             executed_webGL_output = true;
-
             webGLOutputDiv = gradioApp().querySelector('#WebGL-output');
-
             initWebGLOutput(webGLOutputDiv)
-
             observer.disconnect();
         }
     });
 
-    observer.observe(gradioApp(), { childList: true, subtree: true });
+    observer.observe(gradioApp(), {
+        childList: true,
+        subtree: true
+    });
 })
