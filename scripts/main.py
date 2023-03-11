cc587ef8700101674034a1ef0649574575b6e356
missionfloyd <missionfloyd@users.noreply.github.com>
2023-03-10 22:14:52 -0700

    Improve menu
    All settings now update live - no more update buttons
    Height, width, and scale are now sliders
    Colors now use color pickers (don't know why they're so wide, however)
    Added ground color
    Added ground grid checkbox
    Moved "load model" button, made it primary
    Fixed render div border

--- scripts/main.py
+++ scripts/main.py
@@ -23,22 +23,21 @@ def on_ui_tabs():
         with gr.Row():
             with gr.Column():
                 with gr.Row():
-                    has_ground_page = gr.Checkbox(label="Ground", value=opts.threeDmodel_has_ground)
-                    has_axis_page = gr.Checkbox(label="Axis", value=opts.threeDmodel_has_axis)
-                    width_page = gr.Textbox(label="Width", value=opts.threeDmodel_canvas_width)
-                    height_page = gr.Textbox(label="Height", value=opts.threeDmodel_canvas_height)
-                    color_page = gr.Textbox(label="Background Color", value=opts.threeDmodel_bg_color)
+                    width_page = gr.Slider(label="Width", minimum=64, maximum=2048, value=512, step=64, interactive=True)
                 with gr.Row():
-                    set_canvas_button = gr.Button(value="Set Canvas")
+                    height_page = gr.Slider(label="Height", minimum=64, maximum=2048, value=512, step=64, interactive=True)
                 with gr.Row():
-                    model_scale_page = gr.Textbox(label="Scale", value="1")
+                    has_ground_page = gr.Checkbox(label="Show Ground", value=opts.threeDmodel_has_ground)
+                    has_ground_grid_page = gr.Checkbox(label="Show Grid", value=opts.threeDmodel_has_ground_grid)
+                    has_axis_page = gr.Checkbox(label="Show Axis", value=opts.threeDmodel_has_axis)
+                    color_page = gr.ColorPicker(label="Background Color", value=opts.threeDmodel_bg_color)
+                    ground_color_page = gr.ColorPicker(label="Ground Color", value=opts.threeDmodel_ground_color)
                 with gr.Row():
-                    update_model_button = gr.Button(value="Update Model")
-
+                    model_scale_page = gr.Slider(label="Model Scale", minimum=0, maximum=1, step=0.01, value=1)
+                with gr.Row():
+                    upload_button = gr.Button(value="Load Model", variant="primary")
                 with gr.Row():
                     reset_btn = gr.Button(value="Reset")
-                with gr.Row():
-                    upload_button = gr.Button(value="Upload Model")
                     send_t2t = gr.Button(value="Send to txt2img")
                     send_i2i = gr.Button(value="Send to img2img")
                     select_target_index = gr.Dropdown([str(i) for i in range(opts.control_net_max_models_num)],
@@ -49,19 +48,19 @@ def on_ui_tabs():
                     stop_button = gr.Button(value="Stop")
 
             with gr.Column():
-                canvas_width = opts.threeDmodel_canvas_width + "px"
-                canvas_height = opts.threeDmodel_canvas_height + "px"
+                gr.HTML(f'<div id="WebGL-output" canvas_width="{opts.threeDmodel_canvas_width}" canvas_height="{opts.threeDmodel_canvas_height}" ' +
+                        f'canvas_bg_color="{opts.threeDmodel_bg_color}" canvas_ground_color="{opts.threeDmodel_ground_color}" ' +
+                        f'has_ground="{opts.threeDmodel_has_ground}" has_ground_grid="{opts.threeDmodel_has_ground_grid}" has_axis="{opts.threeDmodel_has_axis}" ' +
+                        f'style="width: {opts.threeDmodel_canvas_height + 2}px; height: {opts.threeDmodel_canvas_width + 2}px; border: 0.5px solid"></div>')
 
-                gr.HTML('<div id="WebGL-output" canvas_width="' + opts.threeDmodel_canvas_width +
-                        '" canvas_height="' + opts.threeDmodel_canvas_height + '" canvas_bg_color="' +
-                        opts.threeDmodel_bg_color + '" has_ground="' + str(opts.threeDmodel_has_ground) + '" has_axis="' +
-                        str(opts.threeDmodel_has_axis) + '" style="width: ' +
-                        canvas_width + '; height: ' + canvas_height + '; border-radius: 0.25rem; border: 0.5px solid"></div>')
-
-        update_model_button.click(None, [model_scale_page], None, _js="(modelScalePage) => {updateModel(modelScalePage)}")
-        set_canvas_button.click(None, [has_ground_page, has_axis_page, width_page, height_page, color_page], None,
-                                _js="(hasGroundPage, hasAxisPage, widthPage, heightPage, colorPage) => "
-                                    "{setCanvasPage(hasGroundPage, hasAxisPage, widthPage, heightPage, colorPage)}")
+        width_page.change(None, [width_page, height_page], None, _js="setCanvasSize")
+        height_page.change(None, [width_page, height_page], None, _js="setCanvasSize")
+        model_scale_page.change(None, [model_scale_page], None, _js="updateModel")
+        has_ground_page.change(None, [has_ground_page], None, _js="setGroundVisible")
+        has_ground_grid_page.change(None, [has_ground_grid_page], None, _js="setGroundGridVisible")
+        has_axis_page.change(None, [has_axis_page], None, _js="setAxisVisible")
+        color_page.change(None, [color_page], None, _js="setBGColor")
+        ground_color_page.change(None, [ground_color_page], None, _js="setGroundColor")
         play_pause_button.click(None, [], None, _js="playAndPause")
         stop_button.click(None, [], None, _js="stop")
         send_t2t.click(None, select_target_index, None, _js="(i) => {getWebGLOutputScreenshot('txt2img', i)}")
@@ -69,20 +68,24 @@ def on_ui_tabs():
         reset_btn.click(None, [], None, _js="restCanvasAndCamera")
         upload_button.click(None, None, None, _js="uploadFile")
 
-    return [(threeDModel_loader, "3d Model Loader", "3dmodel_loador")]
+    return [(threeDModel_loader, "3D Model Loader", "3dmodel_loador")]
 
 def on_ui_settings():
     section = ('3dmodel', "3D Model")
     shared.opts.add_option("threeDmodel_bg_color", shared.OptionInfo(
-        "white", "Canvas Background Color", section=section))
+        "#ffffff", "Canvas Background Color", gr.ColorPicker, {"interactive": True}, section=section))
+    shared.opts.add_option("threeDmodel_ground_color", shared.OptionInfo(
+        "#ffffff", "Canvas Ground Color", gr.ColorPicker, {"interactive": True}, section=section))
     shared.opts.add_option("threeDmodel_canvas_width", shared.OptionInfo(
-        "512", "Canvas Width", section=section))
+        512, "Canvas Width", gr.Slider, {"minimum": 64, "maximum": 2048, "step": 64, "interactive": True}, section=section))
     shared.opts.add_option("threeDmodel_canvas_height", shared.OptionInfo(
-        "512", "Canvas Height", section=section))
+        512, "Canvas Height", gr.Slider, {"minimum": 64, "maximum": 2048, "step": 64, "interactive": True}, section=section))
     shared.opts.add_option("threeDmodel_has_ground", shared.OptionInfo(
-        True, "Has Ground", gr.Checkbox, {"interactive": True}, section=section))
+        True, "Show Ground", gr.Checkbox, {"interactive": True}, section=section))
+    shared.opts.add_option("threeDmodel_has_ground_grid", shared.OptionInfo(
+        True, "Show Grid", gr.Checkbox, {"interactive": True}, section=section))
     shared.opts.add_option("threeDmodel_has_axis", shared.OptionInfo(
-        True, "Has Axis", gr.Checkbox, {"interactive": True}, section=section))
+        True, "Show Axis", gr.Checkbox, {"interactive": True}, section=section))
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
 script_callbacks.on_ui_settings(on_ui_settings)
